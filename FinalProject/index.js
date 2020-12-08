d0899f157f22a249953b5f39ee4b9a4f65bc2d32
TandiseruVini <vinivanisa2108@gmail.com>
2020-12-08 14:32:13 +0800

    Final Project

---
+++ FinalProject/index.js
@@ -0,0 +1,17 @@
+let title = document.getElementsByTagName("h1")[0].innerText;
+
+title = document.getElementById("header2").innerText;
+console.log(title);
+
+let umur = document.getElementById("umur").value;
+console.log(umur);
+
+function helloworld(){
+    let value = document.getElementById("umur").value;
+    document.getElementsByName("p")[1].innerText = value;
+    document.querySelector(".header").stylecolor="red";
+    document.querySelector("header").style.fontsize = "3em";
+    document.querySelector(".header").style.backgroundColor = "yellow";
+
+    let span = document.getElementsByTagName("span")[0];
+}
